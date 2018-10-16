dd330ca70da9ddc23a745d385d058cec3af4fc35
Jihun Kim <starlettkim@gmail.com>
2018-10-16 18:14:52 +0900

    구조 또 업데이트!

--- Prj2/include/table.h
+++ Prj2/include/table.h
@@ -2,7 +2,9 @@
 
 namespace JiDB
 {
+    typedef uint64_t pageid_t;
 
+    // Base 
     enum BASE_TYPE { 
         BPTree, RTree, GiST
     };
@@ -12,19 +14,21 @@ namespace JiDB
     public:
         IndexMgr();
         ~IndexMgr();
-        virtual int    _insert(void * key, const void * value) = 0;
-        virtual void * _find  (void * key) = 0;
-        virtual int    _delete(void * key) = 0;
+
+        virtual int    _insert(const void * key, const void * value) = 0;
+        virtual void * _find  (const void * key) = 0;
+        virtual int    _delete(const void * key) = 0;
     };
 
     // Disk manager class.
     class DiskMgr {
     public:
-        const int PAGE_SZ;
-
+        DiskMgr(char * filename, int page_size);
+        ~DiskMgr();
+    
+    private:
+        int page_size;
         int fd;
-        
-        
     };
 
     // 파일 하나에 대한 관리. 하나의 파일은 하나의 자료구조를 갖는다.
@@ -33,16 +37,19 @@ namespace JiDB
         Table(const char * filename, BASE_TYPE type = BPTree);
         ~Table();
 
-        int _insert(void * key, const void * value);
-        void * _find(void * key);
-        int _delete(void * key);
+        void * _find  (const void * key);
+        int    _insert(const void * key, const void * value);
+        int    _delete(const void * key);
         
     private:
         // TODO: Table info
         // e.g.) Scheme
+        // 하지만 이번 버전에서는 scheme가 고정되어 있다고 가정한다.
+        typedef int64_t      key_t;
+        typedef char *       value_t;
+        typedef const char * c_value_t;
 
         BASE_TYPE type;
-        int fd;
 
         IndexMgr * index_mgr;
         DiskMgr  * disk_mgr;
--- Prj2/src/table.cpp
+++ Prj2/src/table.cpp
@@ -1,5 +1,7 @@
 #include "table.h"
 #include "bpt.h"
+#include <fcntl.h>
+#include <unistd.h>
 
 namespace JiDB
 {
@@ -7,7 +9,7 @@ namespace JiDB
     Table::Table(const char * filename, BASE_TYPE type = BPTree) {
         switch(type) {
         case BPTree:
-            index_mgr = new BPT();
+            // index_mgr = new BPT();
 
         case RTree:
         case GiST:
@@ -15,8 +17,21 @@ namespace JiDB
         }
     }
 
-    int Table::_insert(void * key, const void * value) {
+    Table::~Table() { 
+        delete index_mgr;
+        delete disk_mgr;
+    }
+
+    void * Table::_find(const void * key) {
+        return index_mgr->_find(key);
+    }
+
+    int Table::_insert(const void * key, const void * value) {
         return index_mgr->_insert(key, value);
+    }
+
+    int Table::_delete(const void * key) {
+        return index_mgr->_delete(key);
     }
 
 }
